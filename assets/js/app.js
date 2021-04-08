654b7cd51cb190eee676efa80ae47e2743b77f49
Brie Pfisterer <75340215+Seltsam1@users.noreply.github.com>
2021-04-08 14:15:40 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,17 @@
-// @TODO: YOUR CODE HERE!
+// set size for svg element
+var svgWidth = 960;
+var svgHeight = 500;
+
+// define margins to use for chart width/height
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 60,
+    let: 100
+};
+
+// define height width based on margins
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.left;
+
+// use d3 to select id scatter
